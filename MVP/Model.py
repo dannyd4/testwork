7f3e4b09017032060ebc585acd4706f06ebe9398
Danny_D <tip505@mail.ru>
2024-02-25 12:04:07 +0300

    notebook

---
+++ MVP/Model.py
@@ -0,0 +1,9 @@
+from Core.Infrastructure.Notebook import Notebook
+
+
+class Model:
+	def __init__(self):
+		self.current_book = Notebook()
+
+	def get_current_book(self) -> Notebook:
+		return self.current_book
